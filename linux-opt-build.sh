9680cb2a949f6f2b88b55a65aa5cf1f6eb4684f3
kdavis-mozilla <kdavis@mozilla.com>
2020-03-23 07:13:40 +0100

    Added linux build script and win skeleton build script

---
+++ linux-opt-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BUILD_ARCH=x86-64
+
+mkdir build
+
+cd build
+
+cmake -DBUILD_ARCH=${BUILD_ARCH} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DUSE_STATIC_LIBS=on \
+      -DUSE_SENTENCEPIECE=on \
+      ..
+
+make -j
