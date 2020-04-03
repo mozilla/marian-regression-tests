e3916c5d9a18bf1132da4dd3939548f29992bdb4
kdavis-mozilla <kdavis@mozilla.com>
2020-04-03 08:57:27 +0200

    Exchanged 'realpath -m' for 'readlink -m' as 14.04's realpath has no 'm' option

--- tools/diff.sh
+++ tools/diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[[ "$#" -eq 2 ]] && >&2 echo "Command: $(realpath $0) $(realpath -m $1) $(realpath -m $2)"
+[[ "$#" -eq 2 ]] && >&2 echo "Command: $(realpath $0) $(readlink -m $1) $(readlink -m $2)"
 diff $1 $2
