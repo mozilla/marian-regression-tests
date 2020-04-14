ca2e3adde9562d6e760f80c87561d26753e9e7d0
kdavis-mozilla <kdavis@mozilla.com>
2020-04-14 14:40:55 +0200

    Added WIP windows build

--- win-opt-build.sh
+++ win-opt-build.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+export BUILD_ARCH=x86-64
+
+mkdir build
+
+cd build
+
+cmake .. \
+      -DCOMPILE_CUDA=off \
+      -DBUILD_ARCH=${BUILD_ARCH} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DUSE_STATIC_LIBS=on \
+      -DUSE_SENTENCEPIECE=on \
+      -DCOMPILE_SERVER=on \
+      -DCOMPILE_EXAMPLES=on
+
+make -j2
